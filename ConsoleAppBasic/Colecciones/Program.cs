92effc842b1ed19179e9a441940c02c00399ef98
Ema0502 <ema050296@gmail.com>
2024-03-21 18:35:03 -0300

    colection list example

---
+++ ConsoleAppBasic/Colecciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            List<string> listName = new List<string>();
+
+            Console.WriteLine("Introduce el total de nombres de la lista: ");
+            count = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce uno por uno los nombres para agregar: ");
+            for (int i = 0; i < count; i++)
+            {
+                listName.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("Sus nombres ingresados son: ");
+            listName.ForEach(element => Console.WriteLine("- " + element));
+        }
+    }
+}
