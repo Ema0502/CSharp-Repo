d4837a323a5270242ae5d467108daed20ea51c2b
Ema0502 <ema050296@gmail.com>
2024-04-03 19:00:52 -0300

    example of linkedlist + queue + stack + dictionary

--- ConsoleAppBasic/Colecciones/Program.cs
+++ ConsoleAppBasic/Colecciones/Program.cs
@@ -10,6 +10,7 @@ namespace Colecciones
     {
         static void Main(string[] args)
         {
+            //LIST EXAMPLE
             int count = 0;
             List<string> listName = new List<string>();
 
@@ -24,6 +25,67 @@ namespace Colecciones
 
             Console.WriteLine("Sus nombres ingresados son: ");
             listName.ForEach(element => Console.WriteLine("- " + element));
+
+
+            //LINKEDLIST EXAMPLE
+            int[] arrayNumbersForLL = new int[] { 2, 3, 5, 7 };
+            LinkedList<int> ListNode = new LinkedList<int>();
+
+            foreach (int number in arrayNumbersForLL)
+            {
+                ListNode.AddFirst(number);
+            }
+
+            for (LinkedListNode<int> nodo = ListNode.First; nodo != null; nodo = nodo.Next)
+            {
+                int number = nodo.Value;
+                Console.WriteLine(number);
+            }
+
+            //QUEUE EXAMPLE
+            int[] arrayNumbersForQueue = new int[] { 12,376, 15, 37 };
+            Queue<int> myQueue = new Queue<int>();
+
+            foreach (int element in arrayNumbersForQueue)
+            {
+                Console.WriteLine("agregando el elemento " + element);
+                myQueue.Enqueue(element);
+                Console.WriteLine("elemento agregado, cantidad actual = " + myQueue.Count);
+            }
+            
+            while (myQueue.Count > 0)
+            {
+                Console.WriteLine("quitando el elemento: ");
+                myQueue.Dequeue();
+                Console.WriteLine("cantidad actual = " + myQueue.Count);
+            }
+
+            //STACK EXAMPLE
+            
+            int[] arrayNumbersForStack = new int[] { 12, 376, 15, 37 };
+            Stack<int> numbersStack = new Stack<int>();
+            foreach(int number in arrayNumbersForStack)
+            {
+                numbersStack.Push(number);
+                Console.WriteLine("numero agregado, cantidad actual: " + numbersStack.Count);
+            }
+
+            Console.WriteLine("eliminando numero...");
+            numbersStack.Pop();
+            Console.WriteLine("cantidad actual de elementos en el stack: " + numbersStack.Count);
+            
+            //DICTIONARY EXAMPLE
+
+            Dictionary<string, int> personas = new Dictionary<string, int>();
+            personas.Add("Messi", 36);
+            personas.Add("Hazard", 30);
+            personas["Jordan"] = 40;
+            personas["Yuzu"] = 29;
+            foreach (KeyValuePair<string, int> persona in personas)
+            {
+                Console.WriteLine("Nombre: " + persona.Key + ", Edad " + persona.Value);
+                Console.WriteLine("Nombre: {0}, Edad: {1}", persona.Key, persona.Value);
+            }
         }
     }
 }
