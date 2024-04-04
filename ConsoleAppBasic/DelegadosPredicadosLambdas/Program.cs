79ff0a00a85a3839917cf521fbe42c9a7b4cffe7
Ema0502 <ema050296@gmail.com>
2024-04-04 18:06:18 -0300

    added other example predicate

--- ConsoleAppBasic/DelegadosPredicadosLambdas/Program.cs
+++ ConsoleAppBasic/DelegadosPredicadosLambdas/Program.cs
@@ -29,14 +29,36 @@ namespace DelegadosPredicadosLambdas
             {
                 Console.WriteLine(num);
             }
-        }
 
+            List<Personas> gente = new List<Personas>();
+            Personas P1 = new Personas();
+            P1.Nombre = "messi";
+            P1.Edad = 36;
+            Personas P2 = new Personas();
+            P2.Nombre = "hazard";
+            P2.Edad = 33;
+            Personas P3 = new Personas();
+            P3.Nombre = "colo";
+            P3.Edad = 18;
+            gente.AddRange(new Personas[] { P1, P2, P3});
+
+            Predicate<Personas> MiPredPersonas = new Predicate<Personas>(ExisteMessi);
+
+            bool respExiste = gente.Exists(MiPredPersonas);
+            if (respExiste) Console.WriteLine("el messias existe");
+            else Console.WriteLine("messi no existe, se rompe la realidad");
+        }
+        //Fn
         static bool Pares(int num)
         {
             if (num % 2 == 0) return true;
             return false;
         }
 
-
+        static bool ExisteMessi (Personas persona)
+        {
+            if (persona.Nombre.ToLower() == "messi") return true;
+            return false;
+        }
     }
 }
