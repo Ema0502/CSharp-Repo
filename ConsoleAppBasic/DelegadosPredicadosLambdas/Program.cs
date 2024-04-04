c709a7b9a30cdbc5425be863b48cafbf811e8cdc
Ema0502 <ema050296@gmail.com>
2024-04-04 17:14:58 -0300

    added example predicate

--- ConsoleAppBasic/DelegadosPredicadosLambdas/Program.cs
+++ ConsoleAppBasic/DelegadosPredicadosLambdas/Program.cs
@@ -11,10 +11,32 @@ namespace DelegadosPredicadosLambdas
         delegate void ExampleDelegate();
         static void Main(string[] args)
         {
+            //Delegate
             ExampleDelegate MyDelegate = new ExampleDelegate(Saludo.FnSaludo);
             MyDelegate();
             MyDelegate = new ExampleDelegate(Despedida.FnDespedida);
             MyDelegate();
+
+            //Predicate
+
+            List<int> listPares = new List<int>();
+            listPares.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            Predicate<int> predicatePares = new Predicate<int>(Pares);
+
+            List<int> numPares = listPares.FindAll(predicatePares);
+
+            foreach (int num in numPares)
+            {
+                Console.WriteLine(num);
+            }
         }
+
+        static bool Pares(int num)
+        {
+            if (num % 2 == 0) return true;
+            return false;
+        }
+
+
     }
 }
