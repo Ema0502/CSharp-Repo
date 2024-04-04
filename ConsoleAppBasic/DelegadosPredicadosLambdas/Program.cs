6493e2977f8e1f7098f75f34a2970f0c2765bf38
Ema0502 <ema050296@gmail.com>
2024-04-03 22:20:32 -0300

    initial example delegate

---
+++ ConsoleAppBasic/DelegadosPredicadosLambdas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosPredicadosLambdas
+{
+    internal class Program
+    {
+        delegate void ExampleDelegate();
+        static void Main(string[] args)
+        {
+            ExampleDelegate MyDelegate = new ExampleDelegate(Saludo.FnSaludo);
+            MyDelegate();
+            MyDelegate = new ExampleDelegate(Despedida.FnDespedida);
+            MyDelegate();
+        }
+    }
+}
---
+++ ConsoleAppBasic/DelegadosPredicadosLambdas/Saludo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosPredicadosLambdas
+{
+    internal class Saludo
+    {
+        public static void FnSaludo() 
+        {
+            Console.WriteLine("Un saludo de prueba");
+        }
+    }
+}
