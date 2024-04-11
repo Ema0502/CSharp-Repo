e11c6c0563d245b0266048616cdf84565db0ab0e
Ema0502 <ema050296@gmail.com>
2024-04-11 20:54:46 -0300

    btn example with properties, xaml, c#

--- ConsoleAppBasic/InterfazInicial/MainWindow.xaml.cs
+++ ConsoleAppBasic/InterfazInicial/MainWindow.xaml.cs
@@ -19,6 +19,26 @@ namespace InterfazInicial
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid gridBtn = new Grid();
+            this.Content = gridBtn;
+            Button btn3 = new Button();
+            btn3.Width = 166;
+            btn3.Height = 70;
+            gridBtn.Children.Add(btn3);
+            WrapPanel wrapPanelBtn = new WrapPanel();
+            btn3.Content = wrapPanelBtn;
+
+            TextBlock txtBlock1 = new TextBlock();
+            txtBlock1.Text = "txt 1";
+            txtBlock1.FontSize = 18;
+            wrapPanelBtn.Children.Add(txtBlock1);
+
+            TextBlock txtBlock2 = new TextBlock();
+            txtBlock2.Text = "txt 2";
+            txtBlock2.FontSize = 18;
+            wrapPanelBtn.Children.Add(txtBlock2);
+
         }
     }
 }
