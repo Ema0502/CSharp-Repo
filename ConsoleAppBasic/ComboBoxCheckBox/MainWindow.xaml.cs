95cee1d39f1544c47798b2574396ed9304eecaf0
Ema0502 <ema050296@gmail.com>
2024-04-25 21:10:13 -0300

    combobox example + checkbox example

---
+++ ConsoleAppBasic/ComboBoxCheckBox/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBoxCheckBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Capitales> ListaCapitales = new List<Capitales>();
+            ListaCapitales.Add(new Capitales { NombreCapital = "Madrid"});
+            ListaCapitales.Add(new Capitales { NombreCapital = "Df" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Lima" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Bogota" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Santiago" });
+
+            Capitales.ItemsSource = ListaCapitales;
+        }
+
+        private void TodasC_Checked(object sender, RoutedEventArgs e)
+        {
+            Madrid.IsChecked = true;
+            Lima.IsChecked = true;
+            Df.IsChecked = true;
+            Santiago.IsChecked = true;
+            Bogota.IsChecked = true;
+        }
+
+        private void TodasC_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Madrid.IsChecked = false;
+            Lima.IsChecked = false;
+            Df.IsChecked = false;
+            Santiago.IsChecked = false;
+            Bogota.IsChecked = false;
+        }
+
+        private void Individual_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Madrid.IsChecked == true && Lima.IsChecked == true && Df.IsChecked == true && Santiago.IsChecked == true && Bogota.IsChecked == true)
+            {
+                TodasC.IsChecked = true;
+            }
+            else TodasC.IsChecked = null;
+        }
+
+        private void Individual_NoChecked(object sender, RoutedEventArgs e)
+        {
+            if (Madrid.IsChecked == false && Lima.IsChecked == false && Df.IsChecked == false && Santiago.IsChecked == false && Bogota.IsChecked == false)
+            {
+                TodasC.IsChecked = false;
+            }
+            else TodasC.IsChecked = null;
+        }
+    }
+
+    public class Capitales
+    {
+        public string NombreCapital { get; set; }
+    }
+}
