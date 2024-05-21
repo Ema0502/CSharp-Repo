7e03b02a24430fe6ec9e06916501c48cef883ff6
Ema0502 <ema050296@gmail.com>
2024-05-21 14:51:33 -0300

    create added + orm + testing fns

---
+++ ConsoleAppBasic/CrudLinq/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace CrudLinq
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext dataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //conexion con origen de datos
+            string miConexion = ConfigurationManager.ConnectionStrings["CrudLinq.Properties.Settings.CrudLinqSql"].ConnectionString;
+            //data context, mapea la BBDD y obtiene una representacion de objetos de la db
+            dataContext = new DataClasses1DataContext(miConexion);
+
+            //InsertaEmpresas();
+            InsertaEmpleados();
+        }
+        public void InsertaEmpresas()
+        {
+            //elimina todo en la db para cada prueba
+            dataContext.ExecuteCommand("delete from Empresa");
+
+            Empresa empresaGoogle = new Empresa();
+            empresaGoogle.Nombre = "Google";
+            dataContext.Empresa.InsertOnSubmit(empresaGoogle);
+
+            Empresa empresaTesla = new Empresa();
+            empresaTesla.Nombre = "Tesla";
+            dataContext.Empresa.InsertOnSubmit(empresaTesla);
+
+            dataContext.SubmitChanges();
+            Principal.ItemsSource = dataContext.Empresa;
+        }
+
+        public void InsertaEmpleados()
+        {
+            Empresa empresaGoogle = dataContext.Empresa.First((empresa) => empresa.Nombre.Equals("Google"));
+            Empresa empresaTesla = dataContext.Empresa.First((empresa) => empresa.Nombre.Equals("Tesla"));
+
+            List<Empleado> listaEmpleados = new List<Empleado>();
+            listaEmpleados.Add(new Empleado { Nombre = "Lio", Apellido = "Messi", EmpresaId = empresaGoogle.Id});
+            listaEmpleados.Add(new Empleado { Nombre = "Eden", Apellido = "Hazard", EmpresaId = empresaTesla.Id });
+            listaEmpleados.Add(new Empleado { Nombre = "Isao", Apellido = "Machi", EmpresaId = empresaGoogle.Id });
+            listaEmpleados.Add(new Empleado { Nombre = "Ana", Apellido = "Diaz", EmpresaId = empresaTesla.Id });
+            listaEmpleados.Add(new Empleado { Nombre = "Wei", Apellido = "Ying", EmpresaId = empresaGoogle.Id });
+            listaEmpleados.Add(new Empleado { Nombre = "Maria", Apellido = "Lopez", EmpresaId = empresaTesla.Id });
+
+            dataContext.Empleado.InsertAllOnSubmit(listaEmpleados);
+            dataContext.SubmitChanges();
+
+            Principal.ItemsSource = dataContext.Empleado;
+        }
+    }
+}
