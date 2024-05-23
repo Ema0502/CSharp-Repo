ff8a58f6a4debfcb789ec094507d4b31c4331961
Ema0502 <ema050296@gmail.com>
2024-05-23 14:44:39 -0300

    method to synchronize threads

---
+++ ConsoleAppBasic/EjemploConsolaThreards/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EjemploConsolaThreards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //al ejecutar los hilos, el sistema no cumple estrictamente el mismo tiempo de ejecucion para cada proceso
+            Thread threadExample = new Thread(MetodoSaludo);
+            threadExample.Start();
+            //sincronizacion
+            threadExample.Join();
+
+            Console.WriteLine("Hola mundo, thread");
+            Thread.Sleep(500);
+            Console.WriteLine("Hola mundo, thread");
+            Thread.Sleep(500); 
+            Console.WriteLine("Hola mundo, thread");
+            Thread.Sleep(500);
+        }
+
+        static void MetodoSaludo()
+        {
+            Console.WriteLine("Hola mundo, thread 2");
+            Thread.Sleep(500);
+            Console.WriteLine("Hola mundo, thread 2");
+            Thread.Sleep(500);
+            Console.WriteLine("Hola mundo, thread 2");
+            Thread.Sleep(500);
+        }
+    }
+}
