09c84197d6c1461bdfd96f83767424ab43e31eb0
Ema0502 <ema050296@gmail.com>
2024-05-28 16:24:03 -0300

    added Task ContinueWith example

--- ConsoleAppBasic/ThreadTaskExample/Program.cs
+++ ConsoleAppBasic/ThreadTaskExample/Program.cs
@@ -11,19 +11,14 @@ namespace ThreadTaskExample
   {
     static void Main(string[] args)
     {
-      Task tarea1 = new Task(EjecutarTarea);
-      tarea1.Start();
-
-      Task tarea2 = new Task(() =>
+      //Task tarea1 = new Task(EjecutarTarea);
+      //tarea1.Start();
+      Task tarea1 = Task.Run(() =>
       {
-        for (int i = 0; i < 10; i++)
-        {
-          int idThread = Thread.CurrentThread.ManagedThreadId;
-          Thread.Sleep(1000);
-          Console.WriteLine($"El hilo con ID {idThread} esta ejecutandose");
-        }
+        EjecutarTarea();
       });
-      tarea2.Start();
+
+      Task tarea2 = tarea1.ContinueWith(EjecutarOtraTarea);
 
       Console.ReadLine();
     }
@@ -35,6 +30,15 @@ namespace ThreadTaskExample
         int idThread = Thread.CurrentThread.ManagedThreadId;
         Thread.Sleep(1000);
         Console.WriteLine($"Thread con id {idThread} ejecutando la tarea");
+      }
+    }
+    static void EjecutarOtraTarea(Task confirmation)
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        int idThread = Thread.CurrentThread.ManagedThreadId;
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread con id {idThread} inicia ejecucion...");
       }
     }
   }
